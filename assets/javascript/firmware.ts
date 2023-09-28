477e19fee82cb1c12b196a9954971a658902c9ad
Ksenia Lebedeva <ksenia@bitgamma.com>
2023-09-28 20:38:00 +0900

    added changelog to html

--- assets/javascript/firmware.ts
+++ assets/javascript/firmware.ts
@@ -1,9 +1,28 @@
 import KProJS from "kprojs";
 import TransportWebHID from "kprojs-web-hid";
+import { marked } from 'marked';
 
 if (!('process' in window)) {
   // @ts-ignore
   window.process = {}
+}
+
+let fwI = 0;
+
+function handleFWLoadProgress(transport: any, loadBar: HTMLProgressElement) : void {
+  if (fwI == 0) {
+    fwI = 1;
+    let pBarProgress = 0;
+    transport.on("chunk-loaded", (progress: any) => {
+      if (progress >= loadBar.max) {
+        transport.off("chunk-loaded");
+        i = 0;
+      } else {
+        pBarProgress += progress
+        loadBar.value = pBarProgress;
+      }
+    })
+  }
 }
 
 async function handleFirmwareUpdate() : Promise<void> {
@@ -12,6 +31,8 @@ async function handleFirmwareUpdate() : Promise<void> {
   const fwLoad = document.getElementById("progress-bar") as HTMLProgressElement;
   const mediaPrefixField = document.getElementById("media-prefix") as HTMLInputElement;
   const mediaPrefix = mediaPrefixField.value;
+  const fwChangelogLabel = document.getElementById("release-note-fw-version") as HTMLElement;
+  const fwChangelogText = document.getElementById("release-note-fw-changelog") as HTMLDivElement;
 
   const context = await fetch("./context").then((r) => r.json());
 
@@ -20,25 +41,13 @@ async function handleFirmwareUpdate() : Promise<void> {
 
   fwLoad.max = fw.byteLength;
 
+  console.log(JSON.stringify(changelog));
+
+  fwChangelogLabel.innerHTML = "Version " + context["version"];
+  fwChangelogText.innerHTML = marked.parse(changelog.replaceAll('\r',''));
+
   let transport: any;
   let appEth: any;
-  let fwI = 0;
-
-  function handleFWLoadProgress(transport: any, loadBar: HTMLProgressElement) : void {
-    if (fwI == 0) {
-      fwI = 1;
-      let pBarProgress = 0;
-      transport.on("chunk-loaded", (progress: any) => {
-        if (progress >= loadBar.max) {
-          transport.off("chunk-loaded");
-          i = 0;
-        } else {
-          pBarProgress += progress
-          loadBar.value = pBarProgress;
-        }
-      })
-    }
-  }
 
   updateFirmwareBtn.addEventListener("click", async () => {
     progressBar.classList.remove("kpro_web__display-none");
